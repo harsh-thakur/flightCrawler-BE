0c1f50fd60e9a27850f06884384b50cd95b7d2ad
harsh-thakur <harsh.singh@venturepact.com>
2018-06-19 16:27:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+let router = express.Router();
+var bodyParser= require('body-parser');
+app.use(bodyParser());
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://harshDB:db12345@ds161700.mlab.com:61700/flight-server', function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connected');
+    }
+});
+
+// app.use('/routes',root);
+app.listen(3500,function(){
+    console.log('server is runnig on port 3000');
+});
