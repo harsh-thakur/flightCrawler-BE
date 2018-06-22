2774a9176480a13d2c02430e2f4b10cf784979c5
harsh-thakur <harsh.singh@venturepact.com>
2018-06-22 10:32:41 +0530

    apis added

--- controller/flightController.js
+++ controller/flightController.js
@@ -2,25 +2,118 @@ const request = require('request');
 
 exports.check = async (req,res) => {
     console.log(req.body)
-    
-    const clientId = "V1:8wk6pkvaviov7zav:DEVCENTER:EXT"
-    const secretId = "w4SVh3gJ"
+   
+    //sabre API
+
+    // const clientId = "V1:8wk6pkvaviov7zav:DEVCENTER:EXT"
+    // const secretId = "w4SVh3gJ"
+    // const origin = req.body.origin;
+    // const dest = req.body.dest;
+    // const d = req.body.date;
+    // const accessToken = "T1RLAQJabE/KHP3QsDZo3YMeROADkUQDhhABMh3tqWcHgw9K3TH2IdZSAADAxt75Y/U8x9mvy3dfMVqTJ8X4otnCmPL1WoWFWri5FIl2lt5bCQvavbgsSitFHZ59tAVlSu8dcuwrtiuZl/OEsEbNwEaVihV9j9PsNlgkUsLASWbZGnSXpf8+/JuYmzbcSptHbPhEc+CpEDChUYZqLDiLE0nZkyA1gf/cYAdAh4Ivy+Nm48pmVSdt/jBN2RCpekeQJI2j3p+a2tP4z7Ba61Qp5lFq/r5P14K3AR/e4aGyw56HRLR6dZhbtqZqVayv"
+    // request({
+    //     method: 'GET',
+    //     uri: `https://api-crt.cert.havail.sabre.com/v2/shop/flights/fares?origin=${origin}&destination=${dest}&departuredate=${d}&lengthofstay=15&sortby2=departuretime&pointofsalecountry=IN`,
+    //     headers: {'Authorization': 'Bearer ' + accessToken}
+    //   }, function (error, response, body){
+    //     if(!error && response.statusCode == 200){
+    //      body = JSON.parse(body);
+    //       res.json({
+    //           'success': true,
+    //           data:body
+    //       });
+    //     }
+    //   })
+
+
+    //amadeus API
+
+
+    const apiKey = "WePVAZk1sD4KYLkeHvTppphxhhJFzRGL";
     const origin = req.body.origin;
     const dest = req.body.dest;
     const d = req.body.date;
-    const accessToken = "T1RLAQJabE/KHP3QsDZo3YMeROADkUQDhhABMh3tqWcHgw9K3TH2IdZSAADAxt75Y/U8x9mvy3dfMVqTJ8X4otnCmPL1WoWFWri5FIl2lt5bCQvavbgsSitFHZ59tAVlSu8dcuwrtiuZl/OEsEbNwEaVihV9j9PsNlgkUsLASWbZGnSXpf8+/JuYmzbcSptHbPhEc+CpEDChUYZqLDiLE0nZkyA1gf/cYAdAh4Ivy+Nm48pmVSdt/jBN2RCpekeQJI2j3p+a2tP4z7Ba61Qp5lFq/r5P14K3AR/e4aGyw56HRLR6dZhbtqZqVayv"
+
+
+    let originValue;
+    let destinationValue;
+      // To get the city code
+
+
+
+
+     request({
+      method: 'GET',
+      uri:`https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=${apiKey}&term=${origin}&country=IN`
+    }, function (error, response, body){
+      if(!error && response.statusCode == 200){
+       body = JSON.parse(body);
+       if(body.length == 0){
+         res.json({
+           'success': false,
+           msg: 'Either Source or Destination is wrong'
+         })
+       }
+       else{
+       originValue = body[0].value;
+       }
+      //  console.log(originValue);
+       
+      }
+    })
+
     request({
-        method: 'GET',
-        uri: `https://api-crt.cert.havail.sabre.com/v2/shop/flights/fares?origin=${origin}&destination=${dest}&departuredate=${d}&lengthofstay=15&sortby2=departuretime&pointofsalecountry=IN`,
-        headers: {'Authorization': 'Bearer ' + accessToken}
-      }, function (error, response, body){
-        if(!error && response.statusCode == 200){
-         body = JSON.parse(body);
-          res.json({
-              'success': true,
-              data:body
-          });
-        }
-      })
+      method: 'GET',
+      uri:`https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=${apiKey}&term=${dest}&country=IN`
+    }, function (error, response, body){
+      if(!error && response.statusCode == 200){
+       body = JSON.parse(body);
+       if(body.length == 0){
+        res.json({
+          'success': false,
+          msg: 'Either Source or Destination is wrong'
+        })
+      }
+      else{
+        destinationValue= body[0].value;
+
+      }
+      //  console.log(destinationValue);
+       
+      }
+    })
+    setTimeout(function(){
+      let url = `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=${apiKey}&origin=${originValue}&destination=${destinationValue}&departure_date=${d}&currency=INR`
+      request({
+            method: 'GET',
+            uri: url
+          }, function (error, response, body){
+            if(!error && response.statusCode == 200){
+             body = JSON.parse(body);
+              res.json({
+                  'success': true,
+                  data:body
+              });
+            }
+          })
+
+
+    },3000);
+
+ 
+    
+    // let url = `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=${apiKey}&origin=${origin}&destination=${dest}&departure_date=${d}&currency=INR`
+    // request({
+    //       method: 'GET',
+    //       uri: url
+    //     }, function (error, response, body){
+    //       if(!error && response.statusCode == 200){
+    //        body = JSON.parse(body);
+    //         res.json({
+    //             'success': true,
+    //             data:body
+    //         });
+    //       }
+    //     })
     
 }
